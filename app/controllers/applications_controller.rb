e0c112990e62af82ad9116013359c3b1d592ea0e
Wade Naughton <wadenaughton@gmail.com>
2025-04-30 19:59:25 -0400

    application show page and test

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,10 @@ class ApplicationsController < ApplicationController
   def index
     @applications = Application.all
   end
+
+  def show
+    @application = Application.find(params[:id])
+  end
+
+
 end
