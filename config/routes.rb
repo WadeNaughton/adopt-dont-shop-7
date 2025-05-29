696dd426cfd1aca17dc71cef1c129b26cc3c979b
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-05-29 13:13:59 -0400

    Remove '/search' route

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,4 @@ Rails.application.routes.draw do
   get "applications/new", to: "applications#new"
   get "/applications/:id", to: "applications#show"
   post "/applications", to: "applications#create"
-
-  get "/search", to: "pets#search"
 end
