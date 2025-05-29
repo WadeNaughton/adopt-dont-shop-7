958b39333b5943afbd4d2b68a89b3404950c4e5e
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-05-29 14:55:09 -0400

    Adjust formatting

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,62 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe 'Application show page' do 
-    it "Shows name of applicant" do
-        shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-        application = Application.create(name: 'Wade Smith', address: '123 Main Rd.', city: 'Denver', state: 'CO',
-            zip: '00000', description: "bleh bleh bleh", status: 'In Progress')
-        pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-        app_pet = ApplicationPet.create(application: application, pet: pet_1)
+RSpec.describe 'Application show page' do
+  it 'Shows name of applicant' do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    application = Application.create(name: 'Wade Smith', address: '123 Main Rd.', city: 'Denver', state: 'CO',
+                                     zip: '00000', description: 'bleh bleh bleh', status: 'In Progress')
+    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    ApplicationPet.create(application: application, pet: pet_1)
 
-        visit "/applications"
+    visit '/applications'
 
-        expect(page).to have_content(application.name)
+    expect(page).to have_content(application.name)
 
-        click_link "Wade Smith"
+    click_link 'Wade Smith'
 
-        expect(page).to have_current_path("/applications/#{application.id}")
+    expect(page).to have_current_path("/applications/#{application.id}")
+  end
 
+  it 'details of the applicant' do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    application = Application.create(name: 'Wade Smith', address: '123 Main Rd.', city: 'Denver', state: 'CO',
+                                     zip: '00000', description: 'bleh bleh bleh', status: 'In Progress')
+    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    ApplicationPet.create(application: application, pet: pet_1)
 
-    end
+    visit "/applications/#{application.id}"
 
-    it "details of the applicant" do
+    expect(page).to have_content(application.name)
+    expect(page).to have_content(application.address)
+    expect(page).to have_content(application.city)
+    expect(page).to have_content(application.state)
+    expect(page).to have_content(application.zip)
 
-        shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-        application = Application.create(name: 'Wade Smith', address: '123 Main Rd.', city: 'Denver', state: 'CO',
-            zip: '00000', description: "bleh bleh bleh", status: 'In Progress')
-        pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-        app_pet = ApplicationPet.create(application: application, pet: pet_1)
+    expect(page).to have_content(application.description)
+    expect(page).to have_content(application.status)
 
-        visit "/applications/#{application.id}"
+    expect(page).to have_content(pet_1.name)
+  end
 
-        expect(page).to have_content(application.name)
-        expect(page).to have_content(application.address)
-        expect(page).to have_content(application.city)
-        expect(page).to have_content(application.state)
-        expect(page).to have_content(application.zip)
+  it 'searches for a pet and displays the name of the results' do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    application = Application.create(name: 'Wade Smith', address: '123 Main Rd.', city: 'Denver', state: 'CO',
+                                     zip: '00000', description: 'bleh bleh bleh', status: 'In Progress')
+    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    ApplicationPet.create(application: application, pet: pet_1)
 
-        expect(page).to have_content(application.description)
-        expect(page).to have_content(application.status)
+    visit "/applications/#{application.id}"
 
-        expect(page).to have_content(pet_1.name)
+    expect(page).to have_content('Add a Pet to this Application')
 
-    end
+    fill_in 'Add a Pet to this Application:', with: 'Lucille Bald'
 
-    it "searches for a pet and displays the name of the results" do
-
-
-        shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-        application = Application.create(name: 'Wade Smith', address: '123 Main Rd.', city: 'Denver', state: 'CO',
-            zip: '00000', description: "bleh bleh bleh", status: 'In Progress')
-        pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-        app_pet = ApplicationPet.create(application: application, pet: pet_1)
-
-        visit "/applications/#{application.id}"
-        expect(page).to have_content("Add a Pet to this Application")
-        fill_in "Add a Pet to this Application:", with: "Lucille Bald"
-        click_button "Search"
-        expect(page).to have_current_path("/applications/#{application.id}?query=Lucille+Bald&commit=Search")
-        expect(page).to have_content("Lucille Bald")
-
-    end
+    click_button 'Search'
+    
+    expect(page).to have_current_path("/applications/#{application.id}?query=Lucille+Bald&commit=Search")
+    expect(page).to have_content('Lucille Bald')
+  end
 end
