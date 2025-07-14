ee2071f722ca218b1d654d170f0908ea87282125
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-07-14 16:08:37 -0400

    Add test to check for unchanged submission status without including pet to application

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -136,12 +136,12 @@ RSpec.describe 'Application show page' do
     shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     application = Application.create(name: 'Wade Smith', address: '123 Main Rd.', city: 'Denver', state: 'CO',
                                      zip: '00000', description: 'bleh bleh bleh', status: 'In Progress')
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 15, breed: 'tuxedo cat', name: 'Mr. O. Malley', shelter_id: shelter.id)
-    pet_3 =Pet.create(adoptable: true, age: 1, breed: 'wire-haired pointer', name: 'Roman', shelter_id: shelter.id)
+    Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    Pet.create(adoptable: true, age: 15, breed: 'tuxedo cat', name: 'Mr. O. Malley', shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 1, breed: 'wire-haired pointer', name: 'Roman', shelter_id: shelter.id)
 
     ApplicationPet.create(application: application, pet: pet_3)
-    
+
     visit "/applications/#{application.id}"
 
     fill_in 'Add a Pet to this Application:', with: 'roman'
@@ -152,25 +152,39 @@ RSpec.describe 'Application show page' do
 
     expect(page).to have_current_path("/applications/#{application.id}")
     expect(page).to have_content('You already added this pet to your application')
-
-   
   end
 
   it 'submits application with pet successfully and changes status to Pending' do
-      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     application = Application.create(name: 'Wade Smith', address: '123 Main Rd.', city: 'Denver', state: 'CO',
                                      zip: '00000', description: 'bleh bleh bleh', status: 'In Progress')
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 15, breed: 'tuxedo cat', name: 'Mr. O. Malley', shelter_id: shelter.id)
-    pet_3 =Pet.create(adoptable: true, age: 1, breed: 'wire-haired pointer', name: 'Roman', shelter_id: shelter.id)
+    Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    Pet.create(adoptable: true, age: 15, breed: 'tuxedo cat', name: 'Mr. O. Malley', shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 1, breed: 'wire-haired pointer', name: 'Roman', shelter_id: shelter.id)
 
     ApplicationPet.create(application: application, pet: pet_3)
-    
+
     visit "/applications/#{application.id}"
 
     click_button 'Submit Application'
+
     expect(page).to have_content('Application submitted successfully!')
     expect(page).to_not have_content('In Progress')
     expect(page).to_not have_content('Add Pet to this Application')
+  end
+
+  it 'does not submit application without a pet attached' do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    application = Application.create(name: 'Wade Smith', address: '123 Main Rd.', city: 'Denver', state: 'CO',
+                                     zip: '00000', description: 'bleh bleh bleh', status: 'In Progress')
+    Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    Pet.create(adoptable: true, age: 15, breed: 'tuxedo cat', name: 'Mr. O. Malley', shelter_id: shelter.id)
+    Pet.create(adoptable: true, age: 1, breed: 'wire-haired pointer', name: 'Roman', shelter_id: shelter.id)
+
+    visit "/applications/#{application.id}"
+
+    click_button 'Submit Application'
+
+    expect(page).to have_content('You must add at least ONE (1) pet to this application')
   end
 end
